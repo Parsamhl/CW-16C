ad86b316d42c187056eff4601cb0eb41218cf5c7
parsa <mparsah$@gmail.com>
2024-12-13 14:51:33 +0330

    commit

---
+++ scrypt.js
@@ -0,0 +1,18 @@
+function appendValue(value) {
+    const resultField = document.getElementById('result');
+    resultField.value += value;
+}
+
+function clearResult() {
+    document.getElementById('result').value = '';
+}
+
+function calculateResult() {
+    const resultField = document.getElementById('result');
+    try {
+        resultField.value = eval(resultField.value);
+    } catch (error) {
+        alert('Invalid Expression');
+        clearResult();
+    }
+}
